13b10589d532c716a7068c2e6788ffdab3eb771f
Lucas Cavichioli <lucascavica@gmail.com>
2024-08-07 00:32:26 -0300

    Add project files.

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // POST api/users
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return NoContent();
+        }
+
+        [HttpPut("{id}/profile-picture")]
+        public IActionResult PostProfilePicture(IFormFile file) 
+        {
+            var description = $"File: {file.FileName}, Size: {file.Length}";
+
+            //processar a imagem
+            return Ok(description);
+        }
+    }
+}
---
+++ DevFreela.API/Entities/UserSkill.cs
@@ -0,0 +1,16 @@
+﻿namespace DevFreela.API.Entities
+{
+    public class UserSkill
+    {
+        public UserSkill(User user, Skill skill) : base()
+        {
+            User = user;
+            Skill = skill;
+        }
+
+        public int IdUser { get; private set; }
+        public User User { get; private set; }
+        public int IdSkill { get; private set; }
+        public Skill Skill { get; private set; }
+    }
+}
