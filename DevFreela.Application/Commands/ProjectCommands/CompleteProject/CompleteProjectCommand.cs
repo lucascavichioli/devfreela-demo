cf4992392389f0fefea24b91a69da2b169ca22ef
Lucas Cavichioli <lucascavica@gmail.com>
2024-08-26 19:58:29 -0300

    Implementando CQRS

---
+++ DevFreela.Application/Commands/ProjectCommands/CompleteProject/CompleteProjectCommand.cs
@@ -0,0 +1,14 @@
+﻿using DevFreela.Application.Models;
+using MediatR;
+
+namespace DevFreela.Application.Commands.ProjectCommands.CompleteProject
+{
+    public class CompleteProjectCommand : IRequest<ResultViewModel>
+    {
+        public int Id { get; set; }
+        public CompleteProjectCommand(int id)
+        {
+            Id = id;    
+        }
+    }
+}
